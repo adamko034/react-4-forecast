81bd9b2b8b783b8570782b22114ec6075f475a11
Adam Kotas <adamko034@outlook.com>
2018-03-29 18:23:11 +0200

    weather reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios'
+
 const API_KEY = 'cae8493b9f40123cf63e5c401361db46'
+const API_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+    const url = `${API_URL}&q=${city},pl`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
