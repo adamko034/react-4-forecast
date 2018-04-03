783ed4814d0d9d1b0821074ae2d6f1d97e27811f
Adam Kotas <akotas@future-processing.com>
2018-04-03 11:51:45 +0200

    units and chart labeling

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,20 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import { connect } from "react-redux";
-
+import Chart from '../components/chart'
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
+        const temps = _.map(cityData.list.map(w => w.main.temp), (temp) => temp - 273);
+        const pressures = cityData.list.map(w => w.main.pressure);
+        const humidities = cityData.list.map(w => w.main.humidity);
 
         return (
             <tr key={name}>
                 <td>{name}</td>
+                <td><Chart data={temps} color="red" units="C" /></td>
+                <td><Chart data={pressures} color="green" units="hPa"/></td>
+                <td><Chart data={humidities} color="black" units="%" /></td>
             </tr>
         )
     }
@@ -19,9 +25,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (C)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
